fe2dcd980e90879c754f673d0011523bfe9dd95b
pdzzzn <paddyonpc@gmail.com>
2025-06-26 11:32:43 +0200

    Back to working state with TailwindCSS 4

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   plugins: {
     '@tailwindcss/postcss': {},
-    '@tailwindcss/nesting': {}, // Use the direct package name
-    tailwindcss: {},
-    autoprefixer: {},
   },
 };
